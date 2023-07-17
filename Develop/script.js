aee236f50d740f4744ded1b073a32d91d796d9af
Nedda Elsayed <nedda_e@yahoo.com>
2023-07-16 22:30:07 -0700

    update

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,5 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html.
 
-
-//var past = document.querySelector(".past");
-//var present = document.querySelector(".present");
-//var future = document.querySelector(".future");
 var saveBtn = document.querySelector(".saveBtn");
 var time = document.querySelector(".time-block");
 var currentDate = document.querySelector("#currentDay");
@@ -14,13 +10,8 @@ var note = document.querySelector(".description");
 var today = dayjs();
 $('#currentDay').text(today.format('MM/DD/YYYY'));
 
-//var time = dayjs();
 //$
     // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. 
-
-
-
-
 
 
 var timeNow = today.format('HH');
@@ -72,8 +63,6 @@ $("#hour-9 .description").val(localStorage.getItem("hour-9"));
   
 // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. 
 
-//if (){}
-  
 //HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
   
   
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Work Day Scheduler Starter Code
+![Alt text](Assets/05-third-party-apis-homework-demo.gif)
